f994c31edde953a7e18772ff38661e55ab6cd343
Yelena <ynedelko@gmail.com>
2015-08-14 10:14:55 -0700

    Add specs and passing code to return the description and part of speech for a definition.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,10 +1,15 @@
 class Definition
 
-  define_method(:initialize) do |part_of_speech, decribe|
+  define_method(:initialize) do |part_of_speech, describe|
     @part_of_speech = part_of_speech
+    @describe = describe
   end
 
   define_method(:part_of_speech) do
     @part_of_speech
+  end
+
+  define_method(:describe) do
+    @describe
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -6,6 +6,14 @@ end
 
 describe('#part_of_speech') do
   it ("returns the part of speech of the definition") do
-  test_definition = Definition.new("noun", "moister condensed from the atmosphere that falls visibly in separate drops")
+  test_definition = Definition.new("noun", "moister condensed from the atmosphere")
+  expect(test_definition.part_of_speech()).to(eq("noun"))
+  end
+end
+
+describe('#describe') do
+  it ("returns the description of the definition") do
+  test_definition = Definition.new("noun", "moister condensed from the atmosphere")
+  expect(test_definition.describe()).to(eq("moister condensed from the atmosphere"))
   end
 end
