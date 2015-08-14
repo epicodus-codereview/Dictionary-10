e38f60fc079b2baef26b2d30d5c2c268326faf9c
Yelena <ynedelko@gmail.com>
2015-08-14 14:32:53 -0700

    Add route paths to see a list of all words and adding a new word.

--- app.rb
+++ app.rb
@@ -2,9 +2,26 @@ require ('sinatra')
 require ('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/definition')
+require('./lib/word')
 
 get('/') do
   erb(:index)
+end
+
+get('/words/new') do
+  erb(:words_form)
+end
+
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+post('/words') do
+  name = params.fetch('name')
+  Word.new(name).save()
+  @words = Word.all()
+  erb(:success)
 end
 
 get('/definitions') do
@@ -16,16 +33,27 @@ get('/definitions/new') do
   erb(:definitions_form)
 end
 
-post('/definitions') do
-  part_of_speech = params.fetch('part_of_speech')
-  describe = params.fetch('describe')
-  definition = Definition.new(part_of_speech, describe)
-  definition.save()
-  @definitions = Definition.all()
-  erb(:definitions)
-end
-
 get('/definition/:id') do
   @definition = Definition.find(params.fetch('id').to_i())
   erb(:definition)
+end
+
+get ('/words/:id') do
+  @word =Word.find(params.fetch('id').to_i())
+  erb(:word)
+end
+
+get('/words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:word_definitions_form)
+end
+
+post('/definitions') do
+  part_of_speech = params.fetch('part_of_speech')
+  describe = params.fetch('describe')
+  @definition = Definition.new(part_of_speech, describe)
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  erb(:success)
 end
