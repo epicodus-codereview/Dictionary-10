83352af39988f4ee0e6dbde86e21be3e7411a864
Yelena <ynedelko@gmail.com>
2015-08-14 11:59:14 -0700

    Add views and route funtionality to see a list of all definitions and add a new definition.

--- app.rb
+++ app.rb
@@ -6,3 +6,26 @@ require('./lib/definition')
 get('/') do
   erb(:index)
 end
+
+get('/definitions') do
+  @definitions = Definition.all
+  erb(:definitions)
+end
+
+get('/definitions/new') do
+  erb(:definitions_form)
+end
+
+post('/definitions') do
+  part_of_speech = params.fetch('part_of_speech')
+  describe = params.fetch('describe')
+  definition = Definition.new(part_of_speech, describe)
+  definition.save()
+  @definitions = Definition.all()
+  erb(:definitions)
+end
+
+get('/definition/:id') do
+  @definition = Definition.find(params.fetch('id').to_i())
+  erb(:definition)
+end
