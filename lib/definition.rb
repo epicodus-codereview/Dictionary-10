eba72242e082b48a755a17ca624f5cabf5844073
Yelena <ynedelko@gmail.com>
2015-08-14 10:25:12 -0700

    Add passing spec and code to create an empty array for the definitions, save defintions, and clear out saved definitions in array.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,4 +1,5 @@
 class Definition
+  @@definitions = []
 
   define_method(:initialize) do |part_of_speech, describe|
     @part_of_speech = part_of_speech
@@ -11,5 +12,17 @@ class Definition
 
   define_method(:describe) do
     @describe
+  end
+
+  define_singleton_method(:all) do
+    @@definitions
+  end
+
+  define_method(:save) do
+    @@definitions.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@definitions = []
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require ('definition')
 
 describe(Definition) do
+  before() do
+    Definition.clear()
+  end
 end
 
 describe('#part_of_speech') do
@@ -15,5 +18,27 @@ describe('#describe') do
   it ("returns the description of the definition") do
   test_definition = Definition.new("noun", "moister condensed from the atmosphere")
   expect(test_definition.describe()).to(eq("moister condensed from the atmosphere"))
+  end
+end
+
+describe(".all") do
+  it("it creates an empty array for the definition") do
+    expect(Definition.all()).to(eq([]))
+  end
+end
+
+describe("#save") do
+  it("adds a definition to an array of definitions") do
+  test_definition = Definition.new("noun", "moister condensed from the atmosphere")
+  test_definition.save()
+  expect(Definition.all()).to(eq([test_definition]))
+  end
+end
+
+describe(".clear") do
+  it("clears out all the saved definitions in the array") do
+  Definition.new("noun", "moister condensed from the atmosphere").save()
+  Definition.clear()
+  expect(Definition.all()).to(eq([]))
   end
 end
